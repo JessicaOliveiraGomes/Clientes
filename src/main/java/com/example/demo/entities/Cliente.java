c8bbe4b547efaa6dd3564a2d2d2ab15ef661d3ab
JessicaOliveiraGomes <jessicadeoliveiragomes@hotmail.com>
2023-02-05 15:23:42 -0300

    implementando swagger

--- src/main/java/com/example/demo/entities/Cliente.java
+++ src/main/java/com/example/demo/entities/Cliente.java
@@ -3,14 +3,15 @@ package com.example.demo.entities;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,7 +25,7 @@ public class Cliente {
 	private Long id;
 	private String nome;
 	
-	@Column(unique=true)
+	
 	private String cpf;
 	
 	@OneToOne(cascade = CascadeType.ALL)
